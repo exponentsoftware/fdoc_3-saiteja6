dd628bef798a43d44f5e94670bb2d10012fe67d0
saiteja6 <bendalamsaiteja6@gmail.com>
2021-03-05 19:14:55 +0530

    added/used destructuring to extract data from the given array

--- dayThree-1a.js
+++ dayThree-1a.js
@@ -1,3 +1,12 @@
 //A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destruction the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable.
 
 const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+
+let [name, skills, scores] = student //used array destructuring to extract the data from the array and assigned to new variables
+
+
+// console.log(name)
+// console.log(skills)
+// console.log(scores)
+// console.log(student)
+console.log(name, skills, scores);
