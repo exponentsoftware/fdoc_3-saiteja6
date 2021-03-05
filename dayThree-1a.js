82221ab127b72a2810a3779134eaee0ed19baccc
saiteja6 <bendalamsaiteja6@gmail.com>
2021-03-05 19:05:29 +0530

    added the problem statement and the given array

---
+++ dayThree-1a.js
@@ -0,0 +1,3 @@
+//A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destruction the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable.
+
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
